860b43c02e37d8905780579220418af7f34b287b
21mad <ilmir.abyzov@mail.ru>
2023-06-06 20:39:55 +0300

    Modal was created.

--- src/App.js
+++ src/App.js
@@ -12,7 +12,7 @@ function App() {
             <title/>
             <path d="M114,100l49-49a9.9,9.9,0,0,0-14-14L100,86,51,37A9.9,9.9,0,0,0,37,51l49,49L37,149a9.9,9.9,0,0,0,14,14l49-49,49,49a9.9,9.9,0,0,0,14-14Z" />
           </svg>
-          <img src="https://64.media.tumblr.com/f59e05c9984808ca9f2249917db9de70/tumblr_nu3w6mwpBz1sfet3to1_500.gif" />
+          <img alt="gif" src="https://64.media.tumblr.com/f59e05c9984808ca9f2249917db9de70/tumblr_nu3w6mwpBz1sfet3to1_500.gif" />
         </div>
       </div>
     </div>
